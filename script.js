0ffc6502da8c1b7fb20b4fc9d480c450b8a2b4dc
chiricci6 <chiricci6@gmail.com>
2023-07-09 16:22:52 -0300

    puse javascript y borre casi todo

---
+++ script.js
@@ -0,0 +1,12 @@
+function mostrarImagenes() {
+    var cartelImagenes = document.getElementById("cartelImagenes");
+    cartelImagenes.style.display = "block";
+  }
+  
+  function cerrarCartel() {
+    var cartelImagenes = document.getElementById("cartelImagenes");
+    cartelImagenes.style.display = "none";
+  }
+  
+  
+  
